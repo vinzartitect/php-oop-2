3f573a1867a1df1afa237552302447e71aea0943
VincenzoBorriello89 <vincborr89@gmail.com>
2022-05-11 15:53:57 +0200

    creaz scheda prodotti

---
+++ classes/product.php
@@ -0,0 +1,71 @@
+<?php 
+
+// class che contiene il prodotto tipo
+
+class Product {
+    public $name;
+    public $category;
+    public $price;
+    public $typeOfAnimal;
+    
+    // costruttore
+    function __construct( $_name, $_category, $_price, $_typeOfAnimal ){
+        $this -> setName($_name);
+        $this -> setCategory($_category);
+        $this -> setPrice($_price);
+        $this -> setTypeOfAnimal($_typeOfAnimal);
+    }
+
+
+
+    // lista dei setter
+    public function setName($_name){
+        // possibile validazione dei dati
+        $this -> name = $_name;
+        return $this;
+    }
+
+    public function setCategory($_category){
+        // possibile validazione dei dati
+        $this -> category = $_category;
+        return $this;
+    }
+
+    public function setPrice($_price){
+        // possibile validazione dei dati
+        $this -> price = $_price;
+        return $this;
+    }
+
+    public function setTypeOfAnimal($_typeOfAnimal){
+        // possibile validazione dei dati
+        $this -> typeOfAnimal = $_typeOfAnimal;
+        return $this;
+    }
+
+
+
+    // lista dei getter
+    public function getName(){
+        // possibile validazione dei dati
+        return $this -> name;
+    }
+
+    public function getCategory(){
+        // possibile validazione dei dati
+        return $this -> category;
+    }
+
+    public function getPrice(){
+        // possibile validazione dei dati
+        return $this -> price;
+    }
+
+    public function getTypeOfAnimal(){
+        // possibile validazione dei dati
+        return $this -> typeOfAnimal;
+    }
+
+}
+
+?>
